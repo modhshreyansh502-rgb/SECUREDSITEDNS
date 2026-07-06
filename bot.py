39cab36c01de0539b949d197e2af6d008780a08b
modhshreyansh502-rgb <modhshreyansh502@gmail.com>
2026-07-06 19:09:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -207,7 +207,7 @@ Bot:
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+           model="gemini-2.5-flash"
             contents=prompt
         )
 
